dc52e2e5038f56d8945cdff864f98bfc2992611f
boringmammet <boringmammet@gmail.com>
2018-10-22 22:18:29 -0400

    News modifications

--- src/js/xiv/Lodestone.js
+++ src/js/xiv/Lodestone.js
@@ -1,6 +1,8 @@
 import RaelysAPI from "./RaelysAPI";
 const { shell } = require('electron')
 
+
+
 /**
  * Show lodestone news
  */
@@ -17,11 +19,13 @@ class Lodestone
             const ui = document.getElementById('lodestone');
             response.forEach(post => {
                 const html = document.createElement('div');
-                html.innerHTML = `<a href="${post.url}" target="_blank">${post.title}</a>`
+                html.innerHTML = `<a href="${post.url}" onclick="window.open(this.href,'targetWindow', 'toolbar=no, location=no, menubar=yes, scrollbars=yes, resizable=yes, width=1400px, height=900px'); return false;">${post.title}</a>`
                 ui.appendChild(html);
             });
         });
     }
 }
+
+
 
 export default new Lodestone();
